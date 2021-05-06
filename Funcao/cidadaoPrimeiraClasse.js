e03669f93e3d1f248427cef7e161862d9bc06a8f
Pedro52Santos <51803100+Pedro52Santos@users.noreply.github.com>
2021-05-06 10:45:31 -0400

    Att PrimeiraClasse.js
    Aula sobre funções

--- Funcao/cidadaoPrimeiraClasse.js
+++ Funcao/cidadaoPrimeiraClasse.js
@@ -4,3 +4,32 @@
 //criar forma literal
 function fun1() { }
 
+//Armazenar em uma varável
+const fun2 = function() {}
+
+// Armazenar em um array
+const array = [function(){}, fun1, fun2] 
+// é possivel criar uma função no array, puxar uma função literal e puxar uma variável que armazena uma função.
+
+//Armazenar em um atributo de objeto
+const obj = {}
+obj.falar = function() { return 'Opa'}
+console.log(obj.falar())
+
+//Passar uma função como parametro de outra função
+function run(fun){
+    fun()
+}
+
+// Passando a função que vai ser executada depois
+run(function(){console.log('Executando....')}) 
+
+//Uma função pode retornar/ conter utra função
+function Soma(a, b){
+    return function(c){
+        console.log(a + b + c)
+    }
+}
+Soma(2,3)(4) //parametro de soma e logo após parametro de C
+
+
