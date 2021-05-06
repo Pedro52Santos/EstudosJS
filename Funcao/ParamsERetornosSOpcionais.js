66fd46eccde047bc807caad1058fb9f29077ad41
Pedro52Santos <51803100+Pedro52Santos@users.noreply.github.com>
2021-05-06 11:38:39 -0400

    Diferentes Retornos Para função

---
+++ Funcao/ParamsERetornosSOpcionais.js
@@ -0,0 +1,18 @@
+// Cuidado ao explorar retornos diferentes pra função.
+function area(largura, altura){
+    const area = largura * altura
+    if(rea>20){
+        console.log(`Valor acima do permitido ${area}m².`)
+    }else
+    {
+        return area
+    }
+}
+
+
+//todos exemplos ue a linguagem suporta, porém não é aconselhavel usar alguns...
+console.log(area(2,2))
+console.log(area(2))
+console.log(area())
+console.log(area(2,3,4,434))
+console.log(area(5,5))
